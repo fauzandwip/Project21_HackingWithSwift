55fbbba0423cb40fbd1a44a4459191e650b312bc
fauzandp <fauzan27dp@gmail.com>
2023-05-11 13:39:35 +0700

    Project21 | Local Notification App add Notification Categories add button to NotificationCategories manage the didReceive method from userNotificationCenter to handle the data and identifier

--- Project21/ViewController.swift
+++ Project21/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +30,11 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        registerCategories()
+        
         let center = UNUserNotificationCenter.current()
+        // to cancel pending notifications
+        center.removeAllPendingNotificationRequests()
         
         let content = UNMutableNotificationContent()
         content.title = "Late wake up call"
@@ -43,12 +47,50 @@ class ViewController: UIViewController {
         dateCompontents.hour = 11
         dateCompontents.minute = 56
         
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateCompontents, repeats: true)
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateCompontents, repeats: true)
+        
+        // timeInterval in second
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         
         center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let showMore = UNNotificationAction(identifier: "showMore", title: "More, more, more...", options: .destructive)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, showMore], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default identifier")
+            case "show":
+                // the user tapped our "show more info…" button
+                print("Show more information...")
+            case "showMore":
+                // the user tapped our "show more info…" button
+                print("Show more, more, more...")
+            default:
+                break
+            }
+        }
+        
+        // you must call the completion handler when you're done
+        completionHandler()
     }
 
 }
